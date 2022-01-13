893bd02ff7eb643916d23da7649073a002a24aca
kry <kry@DESKTOP-2IF154T>
2022-01-13 13:58:44 +0900

    백준 2748, 1463 문제풀이

---
+++ Bakjoon/src/Bak1463.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bak1463 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(recur(N, 0));
+
+	}
+	
+	static int recur(int N, int count) {
+		if(N < 2) {
+			return count;
+		}
+		return Math.min(recur(N / 2, count + 1 + (N % 2)), recur(N / 3, count + 1 + (N % 3)));
+	}
+
+}
