28b26baf5ba5b8a74e15b42837950cb414d32be7
raeyoung <raeyoung233@gmail.com>
2022-01-27 17:24:07 +0900

    백준 10926, 18108, 2588 문제풀이

---
+++ Bakjoon/src/Bak10926.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ 
+public class Bak10926 {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in); 
+        String id = in.next(); 
+        System.out.println(id + "??!");
+	}
+ 
+}
